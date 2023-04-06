003aa93b1dd80dc39e0da8455e1296f669408664
Olivier Sasseville <olivier.sasseville@termlynx.com>
2023-04-06 17:09:54 -0400

    Deserializing CSV rows into our own Rust structs and types

--- crates/upload-pokemon-data/src/main.rs
+++ crates/upload-pokemon-data/src/main.rs
@@ -1,9 +1,12 @@
+mod pokemon_csv;
+use pokemon_csv::*;
+
 fn main() -> Result<(), csv::Error> {
     let path = "./crates/upload-pokemon-data/pokemon.csv";
     let mut rdr = csv::Reader::from_path(path)?;
 
-    for result in rdr.records() {
-        let record = result?;
+    for result in rdr.deserialize() {
+        let record: PokemonCsv = result?;
         println!("{:?}", record);
     }
 
