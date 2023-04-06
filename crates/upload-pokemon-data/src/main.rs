89644d7d550f02572221a801d8111b401fbf97ec
Olivier Sasseville <olivier.sasseville@termlynx.com>
2023-04-06 16:45:19 -0400

    Reading CSV rows into Rust using the csv crate

--- crates/upload-pokemon-data/src/main.rs
+++ crates/upload-pokemon-data/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), csv::Error> {
+    let path = "./crates/upload-pokemon-data/pokemon.csv";
+    let mut rdr = csv::Reader::from_path(path)?;
+
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
 }
